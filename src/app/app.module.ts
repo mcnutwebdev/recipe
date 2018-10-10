a51fc7db84c815d6bf018fbdf88306ba958b41d3
mcnutwebdev <mcnutwebdev@gmail.com>
2018-10-10 22:37:44 +0100

    Initial commit. Create app. Import FormsModule and HttpModule from angular. Add bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import FormsModule and HttpModule
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +12,11 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  // Add FormsModule and HttpModule to imports array
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
