e672699ee083104ed0ef745f2f5e0ea64dd31d73
mcnutwebdev <mcnutwebdev@gmail.com>
2018-10-11 00:31:29 +0100

    Outputting list of recipes with ngFor.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from './../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')]
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.logolynx.com/images/logolynx/s_82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.logolynx.com/images/logolynx/s_82/829ba7822e43ebe89394d1ecbbf152b7.jpeg'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.logolynx.com/images/logolynx/s_82/829ba7822e43ebe89394d1ecbbf152b7.jpeg')]
   ;
 
   constructor() { }
